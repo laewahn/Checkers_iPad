a0ffe507e92e79666d4b6b65cf849f17326a0e92
Dennis Lewandowski <dennis.lewandowski@rwth-aachen.de>
2014-02-28 19:05:38 +0100

    Initial Commit

---
+++ Checkers_iPad/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Checkers_iPad
+//
+//  Created by Dennis Lewandowski on 28/02/14.
+//  Copyright (c) 2014 laewahn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Checkers_iPadTests/Checkers_iPadTests.m
@@ -0,0 +1,34 @@
+//
+//  Checkers_iPadTests.m
+//  Checkers_iPadTests
+//
+//  Created by Dennis Lewandowski on 28/02/14.
+//  Copyright (c) 2014 laewahn. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Checkers_iPadTests : XCTestCase
+
+@end
+
+@implementation Checkers_iPadTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
